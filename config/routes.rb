ca45a4f4536fbcf3776d73ec0813313421e5af7b
Emmanuel Carre <emma1601@live.com>
2023-01-28 17:43:57 +0400

    venues_index_frontend

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :venues
   resources :venues do
     resources :reviews, only: [:index, :new, :create, :show]
     resources :bookings
