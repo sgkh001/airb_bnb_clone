d189592b1f495a313a14d2ee497aa3e53e2550af
SHAHEEN GOKHOOL <bshine772@gmail.com>
2023-02-02 21:56:34 +0400

    booking status added to schema, routes/controller for confrim/cancel booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_154808) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_145010) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_26_154808) do
     t.bigint "venue_id"
     t.bigint "user_id"
     t.integer "number_of_guest"
+    t.boolean "bookingstatus"
     t.index ["user_id"], name: "index_bookings_on_user_id"
     t.index ["venue_id"], name: "index_bookings_on_venue_id"
   end
