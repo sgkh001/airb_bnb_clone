cf5b75a3b7902a6645fbbd39feb20ee9ec80371c
SHAHEEN GOKHOOL <bshine772@gmail.com>
2023-02-02 13:28:23 +0400

    added javascript to booking index for  booking status function confirm/cancel

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,25 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["confirm", "cancel", "text", "color"]
   connect() {
-    this.element.textContent = "Hello World!"
+    // this.element.textContent = "Hello World!"
+    console.log("Hello from hello controller")
+  }
+  confirm (event) {
+    console.log(event)
+    // this.confirmTarget.setAttribute("disabled", "")
+    // this.cancelTarget.setAttribute("disabled", "")
+    this.textTarget.textContent = "BOOKING CONFIRMED"
+    this.colorTarget.classList.add("bg-success")
+    this.confirmTarget.classList.add("d-none")
+    this.cancelTarget.classList.add("d-none")
+  }
+  cancel (event) {
+    console.log(event)
+    this.textTarget.textContent = "BOOKING CANCELLED"
+    this.colorTarget.classList.add("bg-danger")
+    this.confirmTarget.classList.add("d-none")
+    this.cancelTarget.classList.add("d-none")
   }
 }
